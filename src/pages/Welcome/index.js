c932106248d05e448b26900e3606acba1529473e
Raphael Laurentino da Silva <77465404+RaphaelTW@users.noreply.github.com>
2023-11-17 17:14:59 -0300

    primeiro commit

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+export default function Welcome() {
+ return (
+   <View style={styles.container}>
+        <View style={styles.containerLogo}>
+            <Image
+            source={require('../../assets/logo.png')}
+            style={{ width: '100%' }}
+            resizeMode="contain"
+            />
+        </View>
+
+        <View style={styles.containerForm}>
+            <Text style={styles.title}>Monitore, organize sus gastos de qualquer lugar!</Text>
+            <Text style={styles.text}>Faça o login para começar</Text>
+
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Acessar</Text>
+            </TouchableOpacity>
+        </View>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#A3369A'
+  },
+  containerLogo:{
+    flex: 2,
+    backgroundColor: '#A3369A',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  containerForm:{
+    flex: 1,
+    backgroundColor: '#FFF',
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    paddingStart: '5%',
+    paddingEnd: '5%'
+  },
+  title:{
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 28,
+    marginBottom: 12,
+  },
+  text:{
+
+  },
+  button:{
+
+  },
+  buttonText:{
+
+  }
+})
