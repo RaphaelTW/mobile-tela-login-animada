bd6d96e4d8b06317334fde825db6c033081966e3
Raphael Laurentino da Silva <77465404+RaphaelTW@users.noreply.github.com>
2023-11-17 17:59:32 -0300

    animação tela principal

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,65 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import * as Animatable from 'react-native-animatable'
 
 export default function SignIn() {
  return (
-   <View>
-        <Text>Login</Text>
+   <View style={StyleSheet.container}>
+        <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
+            <Text style={styles.message}>Bem-vindo(a)</Text>
+        </Animatable.View>
+
+        <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+            <Text style={styles.title}>Email</Text>
+              <TextInput
+                placeholder='Digite um email...'
+                style={styles.input}
+              />
+
+            <Text style={styles.title}>Senha</Text>
+              <TextInput
+                placeholder='Sua senha'
+                style={styles.input}
+              />
+
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Acessar</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.buttonRegister}>
+                <Text style={styles.registerText}>Cadastre-se</Text>
+              </TouchableOpacity>
+        </Animatable.View>
    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+
+  },
+  containerHeader:{
+
+  },
+  message:{
+
+  },
+  containerForm: {
+
+  },
+  input:{
+
+  },
+  button:{
+
+  },
+  buttonText:{
+
+  },
+  buttonRegister:{
+
+  },
+  registerText:{
+    
+  }
+})
